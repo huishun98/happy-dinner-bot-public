6feaa70e7f44be92e246d0800c08fa8eaebfbfe0
Chua Hui Shun <huishun@dhcp-206-87-96-68.ubcvisitor.wireless.ubc.ca>
2019-09-09 19:21:41 -0700

    removed credentials

--- common/config.js
+++ common/config.js
@@ -1,6 +1,6 @@
-var localMongo = "mongodb://localhost:27017/dinner-bot"
-const token = '620276831:AAEqU2gLfNIpORu1q1_biz7hyuGXkECVGQ4';
-const appUrl = process.env.APP_URL || "http://hs-dinner-bot.herokuapp.com"
-const giphyApiKey = "wGNDgKWArnt97x7z5suKLwes8nuRRlZf"
+var localMongo = ""
+const token = "";
+const appUrl = process.env.APP_URL || ""
+const giphyApiKey = ""
 
 module.exports = {localMongo, token, appUrl, giphyApiKey};
